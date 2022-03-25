7b44903424e6a0ae909bdb85e53b30a442753231
allan <allansaleh95@gmail.com>
2022-03-25 20:42:09 +0300

    added an audio tag in the player component and made a reference to it

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <TrackInfo currentTrack={currentTrack} />
-      <Player />
+      <Player currentTrack={currentTrack} />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -5,7 +6,11 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentTrack }) => {
+  const audioRef = useRef(null);
+  const playSongHandler = () => {
+    console.log(audioRef.current);
+  };
   return (
     <div className="player-container">
       <div className="duration-bar">
@@ -19,9 +24,15 @@ const Player = () => {
           className="previous-track"
           size="2x"
         />
-        <FontAwesomeIcon icon={faPlay} className="play" size="2x" />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          icon={faPlay}
+          className="play"
+          size="2x"
+        />
         <FontAwesomeIcon icon={faAngleRight} className="next-track" size="2x" />
       </div>
+      <audio ref={audioRef} src={currentTrack.audio} />
     </div>
   );
 };
