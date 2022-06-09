678e847b4f87ef61df618f073366a0cf50057192
allan <allansaleh95@gmail.com>
2022-06-09 19:14:55 +0300

    ➕ added the name as the alt for the cover image

--- src/components/TrackInfo.js
+++ src/components/TrackInfo.js
@@ -1,7 +1,7 @@
 const TrackInfo = ({ currentTrack }) => {
   return (
     <div className="track-info-container">
-      <img src={currentTrack.cover} />
+      <img alt={currentTrack.name} src={currentTrack.cover} />
       <h2>{currentTrack.name}</h2>
       <h3>{currentTrack.artist}</h3>
     </div>
