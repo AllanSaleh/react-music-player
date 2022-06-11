fbb17cae7118f8f22b05ea0a321a05a7deecd098
allan <allansaleh95@gmail.com>
2022-06-11 18:55:57 +0300

    ✨ added styling to the library and made it look a lot better

--- src/components/LibraryTrack.js
+++ src/components/LibraryTrack.js
@@ -2,8 +2,10 @@ const LibraryTrack = ({ track }) => {
   return (
     <div className="library-track">
       <img alt={track.name} src={track.cover} />
-      <h3>{track.name}</h3>
-      <h4>{track.artist}</h4>
+      <div className="track-details">
+        <h3>{track.name}</h3>
+        <h4>{track.artist}</h4>
+      </div>
     </div>
   );
 };
